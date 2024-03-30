0dd6c71ce91c6753d87ad02a63fca0a351f31427
Garrett HE <garrett.he@outlook.com>
2024-03-30 10:14:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# copier-meta
+
+![license](https://img.shields.io/github/license/garrett-he/copier-meta)
+![test](https://img.shields.io/github/actions/workflow/status/garrett-he/copier-meta/test.yml)
+
+Meta template for [Copier][1] allows you to quickly generate specific templates.
+
+## Quickstart
+
+1. Install the latest Copier via command:
+    ```
+    pip install --user --upgrade copier copier-templates-extensions
+    ```
+
+2. Use command `copier` to generate a new project:
+    ```
+    copier copy --trust gh:garrett-he/copier-meta /path/to/destination
+    ```
+
+## License
+
+Copyright (C) 2024 Garrett HE <garrett.he@outlook.com>
+
+The BSD 3-Clause License, see [LICENSE](./LICENSE).
+
+[1]: https://github.com/copier-org/copier
---
+++ copier/jinja_extensions.py
@@ -0,0 +1,40 @@
+import re
+import subprocess
+import unicodedata
+from datetime import date
+
+from jinja2.ext import Extension, Environment
+
+
+def slugify_filter(value: str, separator: str = '-') -> str:
+    value = unicodedata.normalize('NFKD', str(value)).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-_\s]+', separator, value).strip('-_')
+
+
+# pylint: disable=abstract-method
+class DateExtension(Extension):
+    def __init__(self, environment: Environment):
+        super().__init__(environment)
+
+        today = date.today()
+        environment.globals['current_year'] = today.year
+        environment.globals['current_month'] = today.month
+        environment.globals['current_day'] = today.day
+
+
+# pylint: disable=abstract-method
+class GitExtension(Extension):
+    def __init__(self, environment: Environment):
+        super().__init__(environment)
+
+        environment.globals['git_user_name'] = subprocess.getoutput('git config user.name').strip()
+        environment.globals['git_user_email'] = subprocess.getoutput('git config user.email').strip()
+
+
+# pylint: disable=abstract-method
+class SlugifyExtension(Extension):
+    def __init__(self, environment: Environment):
+        super().__init__(environment)
+
+        environment.filters['slugify'] = slugify_filter
