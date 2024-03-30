f12a4a90e149222bccf9a2f89746791af9a6d3e4
Garrett HE <garrett.he@outlook.com>
2024-03-30 11:07:49 +0800

    Meta-template: Add pyproject.toml file

--- tests/test_meta_template.py
+++ tests/test_meta_template.py
@@ -1,5 +1,6 @@
 import random
 
+import toml
 from chance import chance
 from pytest_copie.plugin import Copie
 
@@ -18,12 +19,24 @@ def generate_copier_answers():
     return {
         'template_name': f'{chance.word()}-{chance.word()}',
         'template_description': chance.sentence(),
+        'template_version': f'{random.randint(0, 10)}.{random.randint(0, 10)}.{random.randint(0, 10)}',
+        'template_keywords': f'{chance.word()},{chance.word()},{chance.word()}',
         'copyright_license': chance.pickone(list(LICENSE_SPEC.keys())),
         'copyright_holder_name': chance.name(),
         'copyright_holder_email': chance.email(),
         'copyright_year': str(random.randint(2000, 2024)),
         'vcs_github_path': f'{chance.word()}/{chance.word()}-{chance.word()}'.lower(),
     }
+
+
+def get_license_file(license_id: str) -> str:
+    match license_id:
+        case 'GPL-3.0-or-later' | 'LGPL-3.0-or-later':
+            return 'COPYING'
+        case 'Unlicense':
+            return 'UNLICENSE'
+        case _:
+            return 'LICENSE'
 
 
 def test_meta_template_static_files(copie: Copie):
@@ -54,19 +67,7 @@ def test_meta_template_licenses(copie: Copie):
             assert f'{answers["copyright_holder_name"]} <{answers["copyright_holder_email"]}>' in license_text
             assert answers['copyright_year'] in license_text
 
-        match license_id:
-            case 'GPL-3.0-or-later' | 'LGPL-3.0-or-later':
-                assert result.project_dir.joinpath('COPYING').exists()
-                assert not result.project_dir.joinpath('LICENSE').exists()
-                assert not result.project_dir.joinpath('UNLICENSE').exists()
-            case 'Unlicense':
-                assert not result.project_dir.joinpath('COPYING').exists()
-                assert not result.project_dir.joinpath('LICENSE').exists()
-                assert result.project_dir.joinpath('UNLICENSE').exists()
-            case _:
-                assert not result.project_dir.joinpath('COPYING').exists()
-                assert result.project_dir.joinpath('LICENSE').exists()
-                assert not result.project_dir.joinpath('UNLICENSE').exists()
+        assert result.project_dir.joinpath(get_license_file(license_id)).exists()
 
         readme = result.project_dir.joinpath('README.md').read_text()
 
@@ -81,3 +82,28 @@ def test_meta_template_licenses(copie: Copie):
         else:
             assert f'Copyright (C) {answers["copyright_year"]} {answers["copyright_holder_name"]} <{answers["copyright_holder_email"]}>' in readme
             assert f'see [{license_spec["filename"]}](./{license_spec["filename"]}).' in readme
+
+
+def test_meta_template_pyproject_toml(copie: Copie):
+    answers = generate_copier_answers()
+    result = copie.copy(extra_answers=answers)
+
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_dir.is_dir()
+    assert result.project_dir.joinpath('pyproject.toml').exists()
+
+    with open(result.project_dir.joinpath('pyproject.toml'), 'r', encoding='utf-8') as fp:
+        pyproject = toml.loads(fp.read())
+
+    assert pyproject['project']['name'] == answers['template_name']
+    assert pyproject['project']['version'] == answers['template_version']
+    assert pyproject['project']['description'] == answers['template_description']
+    assert pyproject['project']['authors'][0]['name'] == answers['copyright_holder_name']
+    assert pyproject['project']['authors'][0]['email'] == answers['copyright_holder_email']
+    assert pyproject['project']['license']['text'] == answers['copyright_license']
+    assert pyproject['project']['keywords'] == answers['template_keywords'].split(',')
+
+    assert pyproject['project']['urls']['homepage'] == f"https://github.com/{answers['vcs_github_path']}"
+    assert pyproject['project']['urls']['repository'] == f"https://github.com/{answers['vcs_github_path']}.git"
+    assert pyproject['project']['urls']['changelog'] == f"https://github.com/{answers['vcs_github_path']}/blob/main/CHANGELOG.md"
